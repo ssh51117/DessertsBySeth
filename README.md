a6c3ffc86f428fa94641e1994fb0ba9e0877d1e5
ssh51117 <sshi51117@gmail.com>
2026-03-08 23:15:31 -0400

    initial setup

---
+++ README.md
@@ -0,0 +1,35 @@
+### Frontend
+requirements:
+- landing page
+- menu
+- ordering
+- schedule
+- contact
+- recipes maybe
+## pages:
+customer: 
+- landing
+- menu
+- cart
+- checkout
+- contact
+- tracking
+- about
+admin
+- add/edit products
+- update inventory
+- view orders
+- mark orders completed
+### Backend
+need:
+- data store of orders
+- payment processing
+- email / text reminders
+
+
+next.js
+django REST
+postrgreSQL
+
+host on aws
+payments with stripe
---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+
+export default function Header() {
+    return (
+    <header className="flex items-center justify-between min-h-[60px] px-4 bg-background-50 font-sans dark:bg-grey">
+        <div className="flex items-center gap-3">
+            <span className="font-semibold text-black">Menu</span>
+            <span className="font-semibold text-black">Cart</span>
+            <span className="font-semibold text-black">About</span>
+        </div>
+
+        <div className="absolute left-1/2 -translate-x-1/2 text-dark-green font-bold">
+            Desserts By Seth
+        </div>
+
+        <div className="flex items-center gap-3">
+            <Image
+                className="object"
+                src="/Instagram_simple_icon.png"
+                alt="Instagram icon"
+                width={24}
+                height={24}
+                />
+            </div>
+        </header>)
+}
