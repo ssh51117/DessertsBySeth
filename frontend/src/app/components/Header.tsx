fb01c8c9eb5ec3a22770bcefb867907d99f285e6
ssh51117 <sshi51117@gmail.com>
2026-03-11 23:26:08 -0400

    added menu

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Header() {
     return (
     <header className="flex items-center justify-between min-h-[60px] px-4 bg-background-50 font-sans dark:bg-grey">
         <div className="flex items-center gap-3">
-            <span className="font-semibold text-black">Menu</span>
+            <Link href="/menu" className="font-semibold text-black">Menu</Link>
             <span className="font-semibold text-black">Custom Orders</span>
             <span className="font-semibold text-black">Pop-ups</span>
             <span className="font-semibold text-black">Guinea Pigs</span>
-            <span className="font-semibold text-black">Cart</span>
+            <Link href="/cart" className="font-semibold text-black">Cart</Link>
         </div>
-
-        <div className="absolute left-1/2 -translate-x-1/2 text-dark-green font-bold">
+        <Link href="/" className="absolute left-1/2 -translate-x-1/2 text-dark-green font-bold">
             Desserts By Seth
-        </div>
+        </Link>
 
         <div className="flex items-center gap-3">
             <span className="font-semibold text-black">About</span>
